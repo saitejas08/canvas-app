2dc477963e01da40805a475f45ab4738f9191ed2
Sai Teja Alanthrawar <saitejaalanthrawar@gmail.com>
2025-03-11 23:45:22 +0530

    End-points creation
    10. signup, sign-in, create-room end points
    11. added middleware that decode the token and gate the create room end point
    12. Decode the token in the web-socket server as well. Send the token to the web-socket server in a query param for now.

--- canvas-app/apps/ws-server/src/index.ts
+++ canvas-app/apps/ws-server/src/index.ts
@@ -1,9 +1,27 @@
 import { WebSocketServer } from 'ws';
-
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from './config';
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, request) {
   ws.on('error', console.error);
+
+  const url = request.url;
+  if(!url) {
+    return;
+  }
+  const queryParams = new URLSearchParams(url.split('?')[1]);
+  const token = queryParams.get('token') || "";
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+  if(typeof decoded == "string"){
+    ws.close();
+    return;
+  }
+  if(!decoded || !decoded.userId){
+    ws.close();
+    return;
+  }
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
